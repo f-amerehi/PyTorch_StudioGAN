452a59740c7a344735e7a5bfa1aac67184b27547
f_amerehi <f.amerehi@gmail.com>
2023-11-26 22:45:07 +0000

    Fix windows error

---
+++ help.md
@@ -0,0 +1,4 @@
+
+````bash
+ python "./src/main.py" --load_best -sf -sf_num 100 -metrics none -cfg "./src/configs/CIFAR10/DCGAN.yaml" -data "C:\datasets\cifar10" -ckpt "./ckpt" -save "./output/DCGAN"
+````
--- src/loader.py
+++ src/loader.py
@@ -231,8 +231,8 @@ def load_worker(local_rank, cfgs, gpus_per_node, run_name, hdf5_path):
     # load the generator and the discriminator from a checkpoint if possible
     # -----------------------------------------------------------------------------
     if cfgs.RUN.ckpt_dir is not None:
-        if local_rank == 0:
-            os.remove(join(cfgs.RUN.save_dir, "logs", run_name + ".log"))
+        # if local_rank == 0:
+        #     os.remove(join(cfgs.RUN.save_dir, "logs", run_name + ".log"))
         run_name, step, epoch, topk, aa_p, best_step, best_fid, best_ckpt_path, lecam_emas, logger =\
             ckpt.load_StudioGAN_ckpts(ckpt_dir=cfgs.RUN.ckpt_dir,
                                       load_best=cfgs.RUN.load_best,
